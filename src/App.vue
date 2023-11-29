9d1799b29e732868c5f1e53d96c51477c5030884
huangjianmei <920287501@qq.com>
2023-11-29 12:57:49 +0800

    新建分支

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <HelloWorld msg="You did it!" />
 
       <nav>
-        <RouterLink to="/">Homey</RouterLink>
+        <RouterLink to="/">Homey首页</RouterLink>
         <RouterLink to="/about">About</RouterLink>
       </nav>
     </div>
