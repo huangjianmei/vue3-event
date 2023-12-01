b43cfb3c67517db6014a6566f6f04a1e2fe040b0
huangjianmei <920287501@qq.com>
2023-12-01 14:48:07 +0800

    ref和reactive

---
+++ src/01 ref和reactive.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { ref,reactive,computed } from 'vue'
+
+const count = ref(10);
+const date=reactive({age:23})
+const addCount=()=>{
+  count.value+=10;
+  date.age++;
+};
+
+const dateList=ref([1,2,3,5,7,9,11]);
+const dateListComputed=computed(()=>{
+  dateList.value.filter(i>2)
+});
+console.log(dateListComputed,"==dateListComputed")
+</script>
+
+<template>
+
+  <div>count{{count}}</div>
+  <hr/>
+  <div>age:{{date.age}}</div>
+  <hr/>
+  <button @click="addCount">+10</button>
+
+  <hr/>
+  {{dateListComputed}}
+</template>
+
+<style scoped>
+</style>
