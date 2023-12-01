0984f749023c8c1dbc0a78ece64d99398ed83fd5
huangjianmei <920287501@qq.com>
2023-12-01 16:45:10 +0800

    模板引用

---
+++ src/06 模板引用.vue
@@ -0,0 +1,34 @@
+<script setup>
+  // 局部组件（导入进来就能用）
+  import {ref,onMounted} from "vue"
+  import TestCom from "@/components/06 testCom.vue"
+
+  const inp = ref();
+  console.log(inp);
+  onMounted(()=>{
+    // inp.value.focus()
+    handleFocus()
+  });
+  const handleFocus=()=>{
+    inp.value.focus()
+  };
+
+  const testRef=ref(null);
+  const getCom=()=>{
+    console.log(testRef.value.count,"==testRef")
+    testRef.value.sayHi();
+  };
+</script>
+
+<template>
+  <div>
+    <input type="text" ref="inp">
+    <button @click="handleFocus">点击让输入框聚焦</button>
+    <TestCom ref="testRef"></TestCom>
+    <button @click="getCom">获取组件</button>
+  </div>
+  
+</template>
+
+<style scoped>
+</style>
---
+++ src/components/06 testCom.vue
@@ -0,0 +1,19 @@
+<script setup>
+  const count=999;
+  const sayHi=()=>{
+    console.log("你好")
+  }
+  defineExpose({
+    count,
+    sayHi
+  });
+</script>
+<template>
+  <div>
+    我是用于测试的组件----{{count}}
+  </div>
+</template>
+
+<style scoped>
+
+</style>
