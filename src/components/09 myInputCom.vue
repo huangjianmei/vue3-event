a544e8d8e95a4a8c46867eccbe62f03e9961f2c6
huangjianmei <920287501@qq.com>
2023-12-04 13:30:24 +0800

    defineModel

---
+++ src/components/09 myInputCom.vue
@@ -0,0 +1,28 @@
+
+<!--
+原始写法
+<script setup>
+ defineProps({
+  modelValue:String
+ });
+ const emit=defineEmits(['uodate:modelValue']);
+</script>
+<template>
+  <div>
+    <input type="text" :value="modelValue" @input="e=>emit('update:modelValue',e.target.value)"/>
+  </div>
+</template>
+-->
+<script setup>
+import {defineModel} from "vue"
+ const modelValue = defineModel()
+ const emit=defineEmits(['uodate:modelValue']);
+</script>
+<template>
+  <div>
+    <input type="text" :value="modelValue" @input="e=>modelValue=e.target.value"/>
+  </div>
+</template>
+
+<style scoped>
+</style>
