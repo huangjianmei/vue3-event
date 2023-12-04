9f3bc14c926f1cb48dfa6b36cb47e661e026a279
huangjianmei <920287501@qq.com>
2023-12-04 13:10:03 +0800

    defineOptions

--- src/views/login/index.vue
+++ src/views/login/index.vue
@@ -1,17 +1,18 @@
 <!--<script>
 // vue3.3之前都要这样定义，3.3提供了defineOptions宏
   export default{
-    name:"index"
+    name:"LoginIdex"
   }
 </script>-->
 <script setup>
   defineOptions({
-    name:"index"
+    name:"LoginIdex"
   });
 </script>
+
 <template>
   <div>
-    <h1>我是首页</h1>
+    <h1>我是登录页</h1>
   </div>
 </template>
 
--- src/views/register/index.vue
+++ src/views/register/index.vue
@@ -1,17 +1,20 @@
+
 <!--<script>
 // vue3.3之前都要这样定义，3.3提供了defineOptions宏
   export default{
-    name:"index"
+    name:"RegisterIdex"
   }
 </script>-->
+
 <script setup>
-  defineOptions({
-    name:"index"
+defineOptions({
+    name:"LoginIdex"
   });
 </script>
+
 <template>
   <div>
-    <h1>我是首页</h1>
+    <h1>我是注册页</h1>
   </div>
 </template>
 
