06307df9d6c33f527920e001c1c8d3859871cadd
huangjianmei <920287501@qq.com>
2023-12-05 15:38:31 +0800

    仓库使用

---
+++ src/12 仓库.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { useUserStore } from '@/stores/user'
+const userStore = useUserStore()
+const loginIn = () => {
+  userStore.saveToken('hjhkhkkkkkkk')
+}
+const loginOut = () => {
+  userStore.removeToken()
+}
+</script>
+<template>
+  <div>
+    <h1>我是首页</h1>
+    <h2>token:{{ userStore.token }}</h2>
+    <el-button @click="loginIn">登录</el-button>
+    <el-button @click="loginOut">退出</el-button>
+  </div>
+</template>
+
+<style scoped></style>
