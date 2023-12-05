a7e80cecb07449a0382b2f311f7f5c09ad7c56e8
huangjianmei <920287501@qq.com>
2023-12-05 16:45:29 +0800

    添加拦截器

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup>
-import { useUserStore } from '@/stores/user'
+import { useUserStore, useCounterStore } from '@/stores'
 const userStore = useUserStore()
+const counterStore = useCounterStore()
 const loginIn = () => {
   userStore.saveToken('hjhkhkkkkkkk')
 }
@@ -12,8 +13,10 @@ const loginOut = () => {
   <div>
     <h1>我是首页</h1>
     <h2>token:{{ userStore.token }}</h2>
-    <el-button @click="loginIn">登录</el-button>
+    <h2>count:{{ counterStore.count }}</h2>
+    <el-button @click="loginIn" type="primary">登录</el-button>
     <el-button @click="loginOut">退出</el-button>
+    <el-button @click="counterStore.add" type="success">加法</el-button>
   </div>
 </template>
 
---
+++ src/utils/request.js
@@ -0,0 +1,52 @@
+import axios from 'axios'
+import { useUserStore } from '@/store'
+import router from '../router'
+import { ElMessage } from 'element-plus'
+const baseURL = 'http://big-event-vue-api-t.itheima.net'
+
+const instance = axios.create({
+  // 1.基础地址，超时时间
+  baseURL,
+  timeout: 10000
+})
+
+// 添加请求拦截器
+instance.interceptors.request.use(
+  (config) => {
+    // 在发送请求之前做些什么
+    const userStore = useUserStore()
+    if (userStore.token) {
+      config.token.Authorization = userStore.token
+    }
+    return config
+  },
+  function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error)
+  }
+)
+
+// 添加响应拦截器
+instance.interceptors.response.use(
+  (response) => {
+    // code=0 登录成功
+    if (response.data.code === 0) {
+      return response
+    }
+    // 处理登录失败，给出错误提示
+    ElMessage.error(response.data.message || '服务异常')
+    return Promise.reject(response.data)
+  },
+  function (error) {
+    // 处理401 错误
+    // 错误的特殊情况 => 401权限不足 或 token 过期 => 拦截到登录
+    if (error.response?.status === 401) {
+      router.push('/login')
+    }
+    ElMessage.error(error.response.data.message || '服务异常')
+    return Promise.reject(error)
+  }
+)
+
+export default instance
+export { baseURL }
