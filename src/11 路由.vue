c24ada64fa92bb9d221420cc6af4facf83ed052b
huangjianmei <920287501@qq.com>
2023-12-05 15:34:07 +0800

    路由

---
+++ src/11 路由.vue
@@ -0,0 +1,18 @@
+<script setup>
+import { useRouter } from 'vue-router'
+const router = useRouter()
+const goRegister = () => {
+  console.log(router, '=====router')
+  router.push('/register')
+}
+</script>
+<template>
+  <div>
+    <h1>我是首页</h1>
+    <test-demo></test-demo>
+    <el-button @click="$router.push('/login')">登录</el-button>
+    <el-button @click="goRegister">注册</el-button>
+  </div>
+</template>
+
+<style scoped></style>
